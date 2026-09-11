787551ba030ff262d7a921cd43be0c7f7f3d8e82
Devon Bontrager <139504713+dbontr@users.noreply.github.com>
2026-09-11 17:39:15 -0500

    Yield shared-memory QEMU pause with Asyncify

--- runtime-build/qemu/linuxlab-control.c
+++ runtime-build/qemu/linuxlab-control.c
@@ -6,7 +6,6 @@
 #include "sysemu/runstate.h"
 #include "ui/input.h"
 
-#include <emscripten/atomic.h>
 #include <emscripten/emscripten.h>
 
 #define LINUXLAB_TEXT_CAPACITY 4096
@@ -88,11 +87,7 @@ void linuxlab_vcpu_pause_wait(void)
         cpu_get_ticks() - qatomic_read(&linuxlab_elapsed_ticks_offset));
     qatomic_store_release(&linuxlab_pause_waiting, 1);
     while (linuxlab_pause_requested()) {
-        emscripten_atomic_wait_u32(
-            &linuxlab_paused,
-            1,
-            ATOMICS_WAIT_DURATION_INFINITE
-        );
+        emscripten_sleep(1);
     }
     qatomic_set(&linuxlab_virtual_clock_offset,
         cpu_get_clock() - qatomic_read(&linuxlab_frozen_virtual_clock));
