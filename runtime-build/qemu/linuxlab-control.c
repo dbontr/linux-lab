69552c1ef0363731d6fb03fc8dd7b71da0aca7d3
Devon Bontrager <139504713+dbontr@users.noreply.github.com>
2026-09-10 21:55:50 -0500

    Block paused QEMU vCPU with Wasm atomics

--- runtime-build/qemu/linuxlab-control.c
+++ runtime-build/qemu/linuxlab-control.c
@@ -8,8 +8,8 @@
 #include "sysemu/runstate.h"
 #include "ui/input.h"
 
+#include <emscripten/atomic.h>
 #include <emscripten/emscripten.h>
-#include <emscripten/threading.h>
 
 #define LINUXLAB_TEXT_CAPACITY 4096
 
@@ -59,7 +59,11 @@ void linuxlab_vcpu_pause_wait(void)
 
     qatomic_inc(&linuxlab_pause_waiters);
     while (linuxlab_pause_requested()) {
-        emscripten_thread_sleep(1);
+        emscripten_atomic_wait_u32(
+            &linuxlab_paused,
+            1,
+            ATOMICS_WAIT_DURATION_INFINITE
+        );
     }
     qatomic_dec(&linuxlab_pause_waiters);
 }
@@ -118,6 +122,7 @@ static void linuxlab_resume_bh(void *opaque)
 
     cpu_enable_ticks();
     qatomic_store_release(&linuxlab_paused, 0);
+    emscripten_atomic_notify(&linuxlab_paused, EMSCRIPTEN_NOTIFY_ALL_WAITERS);
 }
 static int linuxlab_scan_code(unsigned char ch, bool *shift)
 {
